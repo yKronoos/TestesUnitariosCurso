b50a62b1f51eb908a91219be3a9279b57f8265c1
Adamo Gonçalves Maia <87905208+yKronoos@users.noreply.github.com>
2024-01-18 09:48:27 -0300

    Create Project

---
+++ 05_FirstStepsWithSpring/TestesUnitariosCurso/src/test/java/br/com/ykronnos/testesunitarioscurso/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.ykronnos.testesunitarioscurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
